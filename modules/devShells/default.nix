4c3bd27356fe5bfbf0e2c45a385f761042315ff4
Gleb Gonchar <gelerum@gmail.com>
2026-04-09 15:17:12 +0700

    separate devShells

---
+++ modules/devShells/default.nix
@@ -0,0 +1,14 @@
+{...}: {
+  perSystem = {pkgs, ...}: {
+    devShells.default = pkgs.mkShell {
+      name = "default-dev-shell";
+      nativeBuildInputs = with pkgs; [
+        git
+        alejandra
+      ];
+      shellHook = ''
+        echo ">> Welcome to the 'default' devShell. Tools: git, alejandra."
+      '';
+    };
+  };
+}
--- modules/flake/per-system.nix
+++ modules/flake/per-system.nix
@@ -1,14 +1,3 @@
 {...}: {
   systems = ["x86_64-linux"];
-
-  perSystem = {pkgs, ...}: {
-    # formatter = pkgs.alejandra;
-
-    devShells.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        git
-        alejandra
-      ];
-    };
-  };
 }
